c892cdb1340931559b12844c46a2533257916b0b
ReliefAmbient <shadowbutterfly1925@gmail.com>
2020-01-10 17:39:54 +0900

    Re:プレイヤー関係の準備

--- Brightless/player.cpp
+++ Brightless/player.cpp
@@ -1,5 +1,6 @@
 #include "DxLib.h"
 #include "player.h"
+#include "input.h"
 
 void Player::init()
 {
@@ -17,4 +18,41 @@ void Player::update()
 void Player::draw()
 {
 
+}
+
+void Player::setSpeed(float speedX)
+{
+	this->speedX = speedX;
+}
+
+void Player::setSpeed(float speedX, float speedY)
+{
+	this->speedX = speedX;
+	this->speedY = speedY;
+}
+
+void movePlayer(Player* obj)
+{
+	switch (Input::GetInstance()->GetLeftThumb(PL_1, XINPUT_BUTTON_LEFT_THUMB))
+	{
+	case Before_Upper_Right:
+	case Upper_Right:
+	case After_Upper_Right:
+	case Right:
+	case Before_Lower_Right:
+	case Lower_Right:
+	case After_Lower_Right:
+		obj->setSpeed(PLAYER_SPEED);
+	case Down:
+	case Before_Lower_Left:
+	case Lower_Left:
+	case After_Lower_Left:
+	case Left:
+	case Before_Upper_Left:
+	case Upper_Left:
+	case After_Upper_Left:
+		obj->setSpeed(-PLAYER_SPEED);
+	default:
+		obj->setSpeed(0);
+	}
 }
